a728dbe887cd6310bff223136b44f860b01544de
SamsonCJ <CS3507529000@gmail.com>
2024-05-02 14:19:00 +0800

    simple action:left-right-jump

--- Legacy Fantasy/player.gd
+++ Legacy Fantasy/player.gd
@@ -1,8 +1,33 @@
 extends CharacterBody2D
 
+const RUN_SPEED := 200.0
+const JUMP_VELOCITY := -300.0 # In 2D - Y direction, jump up means -XXX
+
+
 var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
 
 func _physics_process(delta: float) -> void:
+	# left -> -1 , right -> 1
+	# left + right -> 0
+	var direction := Input.get_axis("move_left", "move_right")
+	velocity.x = direction * RUN_SPEED
 	velocity.y += gravity * delta
+
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+		
+	if is_on_floor():
+		if is_zero_approx(direction):
+			animation_player.play("idle")
+		else:
+			animation_player.play("running")
+	else:
+		animation_player.play("jump")
+		
+	if not is_zero_approx(direction):
+		sprite_2d.flip_h = direction < 0
+	
 	move_and_slide()
