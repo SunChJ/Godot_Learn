ee8cd39fc9d8755ac3d51436cef2ff78f97df7df
SamsonCJ <CS3507529000@gmail.com>
2024-05-02 21:16:48 +0800

    StateMachie: rebuild with StateMachine

--- Legacy Fantasy/player.gd
+++ Legacy Fantasy/player.gd
@@ -1,5 +1,14 @@
 extends CharacterBody2D
 
+enum State {
+	IDLE,
+	RUNNING,
+	JUMP,
+	FALL,
+}
+
+const GROUND_STATE := [State.IDLE, State.RUNNING]
+
 const RUN_SPEED := 160.0
 const FLOOR_ACCELERATION := RUN_SPEED / 0.2 # 0~RunSpeed needs 0.2s
 const AIR_ACCELERATION := RUN_SPEED / 0.02 # 0~RunSpeed needs 0.2s
@@ -21,38 +30,85 @@ func _unhandled_key_input(event: InputEvent) -> void:
 		if velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2	
 
-func _physics_process(delta: float) -> void:
-	# left -> -1 , right -> 1
-	# left + right -> 0
+func tick_physics(state: State, delta: float) -> void:
+	match state:
+		State.IDLE:
+			move(delta)
+		
+		State.RUNNING:
+			move(delta)
+			
+		State.JUMP:
+			move(delta)
+			
+		State.FALL:
+			move(delta)
+	
+func move(delta: float)	-> void:
 	var direction := Input.get_axis("move_left", "move_right")
 	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
 	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, acceleration * delta)
 	velocity.y += gravity * delta
 
-	var can_jump := is_on_floor() or coyote_timer.time_left > 0
-	var shouldJump := can_jump and jump_request_timer.time_left > 0
-	
-	if shouldJump:
-		velocity.y = JUMP_VELOCITY
-		coyote_timer.stop()
-		jump_request_timer.stop()
-		
-	if is_on_floor():
-		if is_zero_approx(direction) and is_zero_approx(velocity.x):
-			animation_player.play("idle")
-		else:
-			animation_player.play("running")
-	else:
-		animation_player.play("jump")
-		
 	if not is_zero_approx(direction):
 		sprite_2d.flip_h = direction < 0
 	
-	var was_on_floor := is_on_floor()
 	move_and_slide()
 	
-	if is_on_floor() != was_on_floor:
-		if was_on_floor and not shouldJump:
-			coyote_timer.start()
-		else:
+			
+func get_next_state(state: State) -> State:
+	var can_jump := is_on_floor() or coyote_timer.time_left > 0
+	var shouldJump := can_jump and jump_request_timer.time_left > 0
+	if shouldJump:
+		return State.JUMP
+	
+	var direction := Input.get_axis("move_left", "move_right")
+	var is_still := is_zero_approx(direction) and is_zero_approx(velocity.x)
+	
+	
+	match state:
+		State.IDLE:
+			if not is_on_floor():
+				return State.FALL
+			
+			if not is_still:
+				return State.RUNNING
+		
+		State.RUNNING:
+			if not is_on_floor():
+				return State.FALL
+			if is_still:
+				return State.IDLE
+			
+		State.JUMP:
+			if velocity.y >= 0:
+				return State.FALL
+			
+		State.FALL:
+			if is_on_floor():
+				return State.IDLE if is_still else State.RUNNING
+		
+	return state
+	
+func transition_state(from: State, to: State) -> void:
+	if from not in GROUND_STATE and to in GROUND_STATE:
+		coyote_timer.stop()
+		
+	
+	match to:
+		State.IDLE:
+			animation_player.play("idle")
+		
+		State.RUNNING:
+			animation_player.play("running")
+			
+		State.JUMP:
+			animation_player.play("jump")
+			velocity.y = JUMP_VELOCITY
 			coyote_timer.stop()
+			jump_request_timer.stop()
+			
+		State.FALL:
+			animation_player.play("fall")
+			if from in GROUND_STATE:
+				coyote_timer.start()
