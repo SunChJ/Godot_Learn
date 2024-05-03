e33fc265abaeb3055c5f51405e16c62ad5655571
SamsonCJ <CS3507529000@gmail.com>
2024-05-03 12:51:31 +0800

    WallJump: Add State WALL_JUMP

--- Legacy Fantasy/player.gd
+++ Legacy Fantasy/player.gd
@@ -7,6 +7,7 @@ enum State {
 	FALL,
 	LANDING,
 	WALL_SLIDING,
+	WALL_JUMP,
 }
 
 const GROUND_STATE := [State.IDLE, State.RUNNING, State.LANDING]
@@ -15,6 +16,7 @@ const RUN_SPEED := 160.0
 const FLOOR_ACCELERATION := RUN_SPEED / 0.2 # 0~RunSpeed needs 0.2s
 const AIR_ACCELERATION := RUN_SPEED / 0.02 # 0~RunSpeed needs 0.2s
 const JUMP_VELOCITY := -320.0 # In 2D - Y direction, jump up means -XXX
+const WALL_JUMP_VELOCITY := Vector2(1000,-320)
 
 var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 var is_first_tick := false
@@ -55,6 +57,9 @@ func tick_physics(state: State, delta: float) -> void:
 		State.WALL_SLIDING:
 			move(default_gravity / 3, delta)
 			graphics.scale.x = get_wall_normal().x
+			
+		State.WALL_JUMP:
+			move(0.0 if is_first_tick else default_gravity, delta)
 			
 	is_first_tick = false
 	
@@ -117,9 +122,15 @@ func get_next_state(state: State) -> State:
 				return State.IDLE 
 		
 		State.WALL_SLIDING:
+			if jump_request_timer.time_left > 0:
+				return State.WALL_JUMP
 			if is_on_floor():
 				return State.IDLE
 			if not is_on_wall():
+				return State.FALL
+				
+		State.WALL_JUMP:
+			if velocity.y >= 0:
 				return State.FALL
 	return state
 	
@@ -151,5 +162,16 @@ func transition_state(from: State, to: State) -> void:
 		
 		State.WALL_SLIDING:
 			animation_player.play("wall_sliding")
+			
+		State.WALL_JUMP:
+			animation_player.play("jump")
+			velocity = WALL_JUMP_VELOCITY
+			velocity.x *= get_wall_normal().x
+			jump_request_timer.stop()
 				
+	if to == State.WALL_JUMP:
+		Engine.time_scale = 0.3
+	if from == State.WALL_JUMP:
+		Engine.time_scale = 1.0
+		
 	is_first_tick = true
